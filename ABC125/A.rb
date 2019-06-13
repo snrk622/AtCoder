596d832bcf5fefc1dc6a2079841b0b1a962feb85
snrk622 <s.0622.r@icloud.com>
2019-06-14 02:45:00 +0900

    20190614 1m

---
+++ ABC125/A.rb
@@ -0,0 +1,8 @@
+# Biscuit Generator https://atcoder.jp/contests/abc125/tasks/abc125_a
+
+a = gets.strip.to_i
+b = gets.strip.to_i
+t = gets.strip.to_i
+puts "================"
+
+puts b*((t+0.5)/a).floor
