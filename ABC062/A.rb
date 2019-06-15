ffcb019520048213404aebce78eed2d7f909e586
snrk622 <s.0622.r@icloud.com>
2019-06-15 20:24:03 +0900

    20190615

--- ABC062/A.rb
+++ ABC062/A.rb
@@ -1,24 +1,18 @@
 # Grouping https://atcoder.jp/contests/abc062/tasks/abc062_a
 
-groupA = [1,3,5,7,8,10,12]
-groupB = [4,6,9,11]
-a = gets.strip.to_i
-b = gets.strip.to_i
+x, y = gets.strip.split.map(&:to_i)
 puts '==============='
 
-if a < b then
-    x = a
-    y = b
-else
-    x = b
-    y = a
+
+groups = [[1, 3, 5, 7, 8, 10, 12], [4, 6, 9, 11], 2]
+result = false
+
+for group in groups
+    break if group == 2
+    result = true if group.include?(x) && group.include?(y)
 end
 
-if x === 2 && y === 2 || groupA.include?(x) && groupA.include?(y) || groupB.include?(x) && groupB.include?(y) then
-    puts 'Yes'
-else
-    puts 'No'
-end
+puts result ? "Yes" : "No"
 
 
 
