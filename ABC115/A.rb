5242f29bc1f4c9cfb6e84aa3374bae77f353f78a
snrk622 <s.0622.r@icloud.com>
2019-06-16 01:24:33 +0900

    20190616 3m

---
+++ ABC115/A.rb
@@ -0,0 +1,17 @@
+# Christmas Eve Eve Evehttps://atcoder.jp/contests/abc115/tasks/abc115_a
+
+d = gets.strip.to_i
+puts "================="
+
+case d
+    when 25 then
+        puts "Christmas" 
+    when 24 then
+        puts "Christmas Eve" 
+    when 23 then
+        puts "Christmas Eve Eve"   
+    when 22 then
+        puts "Christmas Eve Eve Eve" 
+    else
+        
+end
