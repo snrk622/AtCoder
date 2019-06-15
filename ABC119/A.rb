25bbff09b5e2982788db58e69340582c56427046
snrk622 <s.0622.r@icloud.com>
2019-06-15 22:08:23 +0900

    20190615 4m

---
+++ ABC119/A.rb
@@ -0,0 +1,7 @@
+Still TBD https://atcoder.jp/contests/abc119/tasks/abc119_a
+
+s = gets.strip
+puts "==============="
+p s
+
+puts s <= "2019/04/30" ? "Heisei" : "TBD"
---
+++ ABC120/A.rb
@@ -0,0 +1,6 @@
+Favorite Sound https://atcoder.jp/contests/abc120/tasks/abc120_a
+
+a, b, c  = gets.strip.split.map(&:to_i)
+puts "================"
+
+puts b / a >= c ? c : b / a
