3565cb1d6dffbd788f3b9d318776c4e6e12ca769
snrk622 <s.0622.r@icloud.com>
2019-07-10 23:15:04 +0900

    20190710

--- ABC130/C.rb
+++ ABC130/C.rb
@@ -1,2 +1,9 @@
 # Rectangle Cutting https://atcoder.jp/contests/abc130/tasks/abc130_c
 
+W, H, x, y = gets.strip.split.map(&:to_i)
+puts "======================"
+
+S = W * H / 2.00000000000
+i = x == W / 2.0 && y == H / 2.0 ? 1 : 0
+
+puts "#{S} #{i}"
